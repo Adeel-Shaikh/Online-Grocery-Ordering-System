ecf4221b8c16b034e3923995b4d4030424dd0a3b
Adeel Shaikh <65450807+Adeel-Shaikh@users.noreply.github.com>
2020-11-02 10:48:22 +0530

    Add files via upload

---
+++ form/footer.php
@@ -0,0 +1,19 @@
+<div class="container-fluid animatedParent" style="background:#F8F9FA; padding:40px;"> 
+		    <div class="container">
+		      
+			      <div class="row animated fadeIn">
+				      
+					  
+					  <div class="col-sm-6" >
+					    <ul>
+                          <li><h5>ADMIN SECTION</h5></li>
+						  <li><a href="../admin.php">Admin Login</a></li>
+						  
+						</ul>
+					  </div>
+					  
+					 
+				  </div>
+			 </div>
+			 			 
+		  </div>
