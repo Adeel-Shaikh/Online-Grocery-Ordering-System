32dfe1c5a84dd5f495ee51ba60ea46bcd636e179
Adeel Shaikh <65450807+Adeel-Shaikh@users.noreply.github.com>
2020-11-02 10:52:19 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$hostname="localhost";
+$user_name="root";
+$password="";
+$db="mini_project";
+$con=mysqli_connect($hostname,$user_name,$password,$db);
+
+ 
+
+?>
---
+++ mini_project.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 22, 2020 at 12:42 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mini_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbadmin`
+--
+
+CREATE TABLE `tbadmin` (
+  `fld_id` int(10) NOT NULL,
+  `fld_username` varchar(30) NOT NULL,
+  `fld_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbadmin`
+--
+
+INSERT INTO `tbadmin` (`fld_id`, `fld_username`, `fld_password`) VALUES
+(1, 'admin', 'admin@123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbfood`
+--
+
+CREATE TABLE `tbfood` (
+  `food_id` int(11) NOT NULL,
+  `fldvendor_id` int(11) NOT NULL,
+  `foodname` varchar(100) NOT NULL,
+  `cost` bigint(15) NOT NULL,
+  `cuisines` varchar(50) NOT NULL,
+  `paymentmode` varchar(50) NOT NULL,
+  `fldimage` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbfood`
+--
+
+INSERT INTO `tbfood` (`food_id`, `fldvendor_id`, `foodname`, `cost`, `cuisines`, `paymentmode`, `fldimage`) VALUES
+(1, 11, 'Apple', 100, 'Fruit', 'COD', 'apple.jpg'),
+(2, 11, 'Orange', 123, 'Fruit', 'COD', 'orange.jpg'),
+(3, 11, 'Pomogrenate', 145, 'Fruit', 'COD', 'pomogrenate.jpg'),
+(4, 10, 'Tomato', 100, 'Vegetable', 'COD', 'tomato.jpg'),
+(5, 10, 'Carrot', 100, 'Vegetable', 'COD', 'carrot.jpg'),
+(6, 10, 'Potato', 45, 'Vegetable', 'COD', 'potato.jpg'),
+(7, 10, 'Onion', 50, 'Vegetable', 'COD', 'onion.jpg'),
+(8, 11, 'Strawberry', 90, 'Fruit', 'COD', 'strawberry.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcart`
+--
+
+CREATE TABLE `tblcart` (
+  `fld_cart_id` int(11) NOT NULL,
+  `fld_product_id` bigint(11) NOT NULL,
+  `fld_customer_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomer`
+--
+
+CREATE TABLE `tblcustomer` (
+  `fld_cust_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(30) NOT NULL,
+  `fld_mobile` bigint(10) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblcustomer`
+--
+
+INSERT INTO `tblcustomer` (`fld_cust_id`, `fld_name`, `fld_email`, `fld_mobile`, `password`) VALUES
+(1, 'gajender', 'customer1@gmail.com', 7503515382, 'customer1'),
+(2, 'sanjay', 'customer2@gmail.com', 7503515386, 'customer2'),
+(3, 'saana', 'customer3@gmail.com', 7503515383, 'customer3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblmessage`
+--
+
+CREATE TABLE `tblmessage` (
+  `fld_msg_id` int(10) NOT NULL,
+  `fld_name` varchar(50) NOT NULL,
+  `fld_email` varchar(50) NOT NULL,
+  `fld_phone` bigint(10) DEFAULT NULL,
+  `fld_msg` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblorder`
+--
+
+CREATE TABLE `tblorder` (
+  `fld_order_id` int(10) NOT NULL,
+  `fld_cart_id` bigint(10) NOT NULL,
+  `fldvendor_id` bigint(10) DEFAULT NULL,
+  `fld_food_id` bigint(10) DEFAULT NULL,
+  `fld_email_id` varchar(50) DEFAULT NULL,
+  `fld_payment` varchar(20) DEFAULT NULL,
+  `fldstatus` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblorder`
+--
+
+INSERT INTO `tblorder` (`fld_order_id`, `fld_cart_id`, `fldvendor_id`, `fld_food_id`, `fld_email_id`, `fld_payment`, `fldstatus`) VALUES
+(1, 1, 21, 1, 'customer3@gmail.com', '50', 'Delivered'),
+(2, 2, 22, 3, 'customer3@gmail.com', '20', 'Out Of Stock');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvendor`
+--
+
+CREATE TABLE `tblvendor` (
+  `fldvendor_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(50) NOT NULL,
+  `fld_password` varchar(50) NOT NULL,
+  `fld_mob` bigint(10) NOT NULL,
+  `fld_phone` bigint(10) NOT NULL,
+  `fld_address` varchar(50) NOT NULL,
+  `fld_logo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblvendor`
+--
+
+INSERT INTO `tblvendor` (`fldvendor_id`, `fld_name`, `fld_email`, `fld_password`, `fld_mob`, `fld_phone`, `fld_address`, `fld_logo`) VALUES
+(10, 'Fresh Vegetable', 'vendor4@gmail.com', 'vendor4', 7503515385, 114565457, 'C-33, SWARN PARK', 'Vegetables.jpg'),
+(11, 'Fresh Fruit', 'vendor3@gmail.com', 'vendor3', 7503515385, 114565457, 'C-33, SWARN PARK', 'Fruits.jpg'),
+(22, 'Radison', 'vendor1@gmail.com', 'vendor1', 7503515386, 114565457, 'noida', '46388969.jpg'),
+(23, 'Piccaso', 'vendor2@gmail.com', 'vendor2', 7503515385, 114565457, 'C-33, SWARN PARK, MUNDKA', '46388969.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD PRIMARY KEY (`fld_id`);
+
+--
+-- Indexes for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `fldvendor_id` (`fldvendor_id`);
+
+--
+-- Indexes for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  ADD PRIMARY KEY (`fld_cart_id`);
+
+--
+-- Indexes for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  ADD PRIMARY KEY (`fld_cust_id`);
+
+--
+-- Indexes for table `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  ADD PRIMARY KEY (`fld_msg_id`);
+
+--
+-- Indexes for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  ADD PRIMARY KEY (`fld_order_id`);
+
+--
+-- Indexes for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  ADD PRIMARY KEY (`fldvendor_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  MODIFY `fld_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  MODIFY `fld_cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  MODIFY `fld_cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  MODIFY `fld_msg_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  MODIFY `fld_order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  MODIFY `fldvendor_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  ADD CONSTRAINT `tbfood_ibfk_1` FOREIGN KEY (`fldvendor_id`) REFERENCES `tblvendor` (`fldvendor_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
